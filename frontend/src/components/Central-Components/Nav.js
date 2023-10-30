82baad04666daac9e93f042a48f9d9c39b35e709
jcabrera2502 <123277286+jcabrera2502@users.noreply.github.com>
2023-10-29 22:20:29 -0400

    Added menu button functionality.

--- frontend/src/components/Central-Components/Nav.js
+++ frontend/src/components/Central-Components/Nav.js
@@ -31,31 +31,85 @@ function Nav() {
     });
     }, []);
 
+    //Side menu
+
+    const [state, setState] = React.useState({
+        menu: false,
+      });
+
+    // Side Menu toggle button
+    const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+    }
+
+    setState({ ...state, [anchor]: open });
+    };
+
+    // Side Menu list
+    const list = (anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List sx={{mt: 8}}>
+            {['Testing', 'Testing', 'Testing', 'Testing'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Testing', 'Testing', 'Testing'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
     return(
-        <AppBar position="fixed">
-            <Drawer>
+        <>
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <nav>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={toggleDrawer("left", true)}
+                            >
+                            <MenuIcon />
+                        </IconButton>
+                        <Box sx={{flexGrow: 1}}></Box>
+                        {user && <Button onClick={() => (window.location.href = "http://localhost:3000/")}>Home</Button>}
+                        {!user && <Button onClick={() => (window.location.href = "http://localhost:3000/SignIn")}>SignIn</Button>}
+                        {!user && <Button onClick={() => (window.location.href = "http://localhost:3000/SignUp")}>SignUp</Button>}
+                        {user && <Button onClick={() => (window.location.href = "http://localhost:3000/AuthDetails")}>Logout</Button>}
+                        {user && <IconButton onClick={() => (window.location.href = "http://localhost:3000/Profile")}><AccountCircleIcon /></IconButton>}
+                        {user && <IconButton onClick={() => (window.location.href = "http://localhost:3000/Settings")}><SettingsIcon /></IconButton>}
+                    </Toolbar>
+                </nav>
+            </AppBar>
+            <Drawer
+                    sx={{flexShrink: 0}}
+                    anchor={"left"}
+                    open={state["left"]}
+                    onClose={toggleDrawer("left", false)}
+                >
+                {list('left')}
             </Drawer>
-            <nav>
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        >
-                        <MenuIcon />
-                    </IconButton>
-                    <Box sx={{flexGrow: 1}}></Box>
-                    {user && <Button onClick={() => (window.location.href = "http://localhost:3000/")}>Home</Button>}
-                    {!user && <Button onClick={() => (window.location.href = "http://localhost:3000/SignIn")}>SignIn</Button>}
-                    {!user && <Button onClick={() => (window.location.href = "http://localhost:3000/SignUp")}>SignUp</Button>}
-                    {user && <Button onClick={() => (window.location.href = "http://localhost:3000/AuthDetails")}>Logout</Button>}
-                    {user && <IconButton onClick={() => (window.location.href = "http://localhost:3000/Profile")}><AccountCircleIcon /></IconButton>}
-                    {user && <IconButton onClick={() => (window.location.href = "http://localhost:3000/Settings")}><SettingsIcon /></IconButton>}
-                </Toolbar>
-            </nav>
-        </AppBar>
+        </>
     );
 }
 
