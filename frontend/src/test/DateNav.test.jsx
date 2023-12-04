f81aa0168bd090919b71d48866a90f388694a1f9
tnk6 <tnk6@njit.edu>
2023-12-03 20:35:04 -0500

    finished datenav tests

--- frontend/src/test/DateNav.test.jsx
+++ frontend/src/test/DateNav.test.jsx
@@ -185,3 +185,426 @@ test('lower day to 30 if new month doesn\'t have 31', () => {
     const look4 = list4.queryByText("31");
     expect(look4).toBeNull();*/
 });
+
+test('lower day to 28 if new month doesn\'t have 30', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list0 = within(screen.getByRole('listbox'));
+    fireEvent.click(list0.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText("30"));
+    expect(screen.getAllByText("30")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list5 = within(screen.getByRole('listbox'));
+    fireEvent.click(list5.getByText("2025"));
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("30")).toBeNull();
+    expect(screen.getAllByText("28")[0]).toBeInTheDocument();
+    /*
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list4 = within(screen.getByRole('listbox'));
+    const look4 = list4.queryByText("31");
+    expect(look4).toBeNull();*/
+});
+
+test('lower day to 29 if new month doesn\'t have 30 and on leap year', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list0 = within(screen.getByRole('listbox'));
+    fireEvent.click(list0.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText("30"));
+    expect(screen.getAllByText("30")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list5 = within(screen.getByRole('listbox'));
+    fireEvent.click(list5.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("30")).toBeNull();
+    expect(screen.getAllByText("29")[0]).toBeInTheDocument();
+    /*
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list4 = within(screen.getByRole('listbox'));
+    const look4 = list4.queryByText("31");
+    expect(look4).toBeNull();*/
+});
+
+test('previous month', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[0];
+    fireEvent.click(button);
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+});
+
+test('next month', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[1];
+    fireEvent.click(button);
+    expect(screen.getAllByText("March")[0]).toBeInTheDocument();
+});
+
+test('previous day', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("15"));
+    expect(screen.getAllByText("15")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[2];
+    fireEvent.click(button);
+    expect(screen.getAllByText("14")[0]).toBeInTheDocument();
+});
+
+test('next day', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("15"));
+    expect(screen.getAllByText("15")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[3];
+    fireEvent.click(button);
+    expect(screen.getAllByText("16")[0]).toBeInTheDocument();
+});
+
+test('previous year', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("2025"));
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[4];
+    fireEvent.click(button);
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+});
+
+test('next year', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("2025"));
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[5];
+    fireEvent.click(button);
+    expect(screen.getAllByText("2026")[0]).toBeInTheDocument();
+});
+
+test('previous month lowers day if needed', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/October/));
+    expect(screen.getAllByText("October")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("31"));
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[0];
+    fireEvent.click(button);
+    expect(screen.getAllByText("September")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("31")).toBeNull();
+    expect(screen.getAllByText("30")[0]).toBeInTheDocument();
+});
+
+test('next month lowers day if needed', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("31"));
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2025"));
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[1];
+    fireEvent.click(button);
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("31")).toBeNull();
+    expect(screen.getAllByText("28")[0]).toBeInTheDocument();
+});
+
+test('previous month lowering day accounts for leap years', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/March/));
+    expect(screen.getAllByText("March")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("30"));
+    expect(screen.getAllByText("30")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[0];
+    fireEvent.click(button);
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("30")).toBeNull();
+    expect(screen.getAllByText("29")[0]).toBeInTheDocument();
+});
+
+test('next month lowering day accounts for leap years', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("31"));
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[1];
+    fireEvent.click(button);
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("31")).toBeNull();
+    expect(screen.getAllByText("29")[0]).toBeInTheDocument();
+});
+
+test('previous month rollover', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[0];
+    fireEvent.click(button);
+    expect(screen.getAllByText("December")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("2023")[0]).toBeInTheDocument();
+});
+
+test('next month rollover', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/December/));
+    expect(screen.getAllByText("December")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[1];
+    fireEvent.click(button);
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+});
+
+test('previous day rollover', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/April/));
+    expect(screen.getAllByText("April")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("1"));
+    expect(screen.getAllByText("1")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[2];
+    fireEvent.click(button);
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+
+    expect(screen.getAllByText("March")[0]).toBeInTheDocument();
+});
+
+test('next day rollover', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/April/));
+    expect(screen.getAllByText("April")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("30"));
+    expect(screen.getAllByText("30")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[3];
+    fireEvent.click(button);
+    expect(screen.getAllByText("1")[0]).toBeInTheDocument();
+
+    expect(screen.getAllByText("May")[0]).toBeInTheDocument();
+});
+
+test('previous day rollover with year change', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/January/));
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("1"));
+    expect(screen.getAllByText("1")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[2];
+    fireEvent.click(button);
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+    
+    expect(screen.getAllByText("December")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("2023")[0]).toBeInTheDocument();
+});
+
+test('next day rollover with year change', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/December/));
+    expect(screen.getAllByText("December")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("31"));
+    expect(screen.getAllByText("31")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[3];
+    fireEvent.click(button);
+    expect(screen.getAllByText("1")[0]).toBeInTheDocument();
+    
+    expect(screen.getAllByText("January")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+});
+
+test('previous year lowers day if needed', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("29"));
+    expect(screen.getAllByText("29")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[4];
+    fireEvent.click(button);
+    expect(screen.getAllByText("2023")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("29")).toBeNull();
+    expect(screen.getAllByText("28")[0]).toBeInTheDocument();
+});
+
+test('next year lowers day if needed', () => {
+    render(<DateNav />);
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[0]);
+    const list1 = within(screen.getByRole('listbox'));
+    fireEvent.click(list1.getByText(/February/));
+    expect(screen.getAllByText("February")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[2]);
+    const list3 = within(screen.getByRole('listbox'));
+    fireEvent.click(list3.getByText("2024"));
+    expect(screen.getAllByText("2024")[0]).toBeInTheDocument();
+
+    fireEvent.mouseDown(screen.getAllByRole('combobox')[1]);
+    const list2 = within(screen.getByRole('listbox'));
+    fireEvent.click(list2.getByText("29"));
+    expect(screen.getAllByText("29")[0]).toBeInTheDocument();
+
+    const button = screen.getAllByRole('button')[5];
+    fireEvent.click(button);
+    expect(screen.getAllByText("2025")[0]).toBeInTheDocument();
+
+    expect(screen.queryByText("29")).toBeNull();
+    expect(screen.getAllByText("28")[0]).toBeInTheDocument();
+});
