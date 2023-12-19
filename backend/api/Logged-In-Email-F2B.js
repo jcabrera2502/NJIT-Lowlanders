bfde11ae28be4bd91bee987268476ddb1bf28ea2
GCity <gcn5@njit.edu>
2023-12-19 13:47:35 -0500

    Got the Cancel feature working

--- backend/api/Logged-In-Email-F2B.js
+++ backend/api/Logged-In-Email-F2B.js
@@ -8,7 +8,6 @@ const router = express.Router();
 //Gets the user information based off of their email DONE
 router.get('/api/email', async (req, res) =>
 {
-    console.log(req);
     const collection = mongoose.connection.db.collection("user-profile");
     const result = await collection.findOne({email: req.query.email})
     res.send(result);
@@ -40,7 +39,6 @@ router.post('/api/new', async (req, res) => {
 router.put('/api/updateProfile', async (req, res) =>
 {
     const collection = mongoose.connection.db.collection("user-profile");
-    console.log("Req" , req.body.params)
     const result = await collection.updateOne({email: req.body.params.email}, {$set: {firstName: req.body.params.firstName, lastName: req.body.params.lastName, pomodoro:req.body.params.pomodoro, shortBreak:req.body.params.shortBreak, longBreak:req.body.params.longBreak}})
     res.send(result);
    
@@ -49,7 +47,6 @@ router.put('/api/updateProfile', async (req, res) =>
 router.post('/api/insertTask', async (req, res) =>
 {
     const collection = mongoose.connection.db.collection("user-tasks");
-    console.log("Req" , req.body.params)
 
     const result = await collection.insertOne({key: req.body.params.key, 
                                                email: req.body.params.email,
@@ -82,7 +79,6 @@ router.get('/api/getTasks', async (req, res) =>
 router.put('/api/updateTask', async (req, res) =>
 {
     const collection = mongoose.connection.db.collection("user-tasks");
-    console.log("Req" , req.body.params)
     const result = await collection.updateOne({email: req.body.params.email, 
                                               taskTitle: req.body.params.title, 
                                               day: req.body.params.day, 
@@ -100,7 +96,6 @@ router.put('/api/updateTask', async (req, res) =>
 router.put('/api/updateTaskStatus', async (req, res) =>
 {
    const collection = mongoose.connection.db.collection("user-tasks");
-   console.log("Req" , req.body.params)
    const result = await collection.updateOne({email: req.body.params.email,
                                               taskTitle: req.body.params.title,
                                               day: req.body.params.day,
@@ -120,6 +115,21 @@ router.get('/api/getTaskPreviousDay', async (req, res) =>
     console.log("REQUEST" , req.query.status)
     const result = await collection.find({email: req.query.email, day: day, month: month, year: year, status: status}).toArray();
     res.send(result);
+});
+
+router.delete('/api/deleteTask', async (req, res) => {
+  const collection = mongoose.connection.db.collection("user-tasks");
+  let email = req.query.email;
+  email = decodeURIComponent(email);
+  const title = req.query.title;
+
+  try {
+      const result = await collection.deleteOne({ email: email, taskTitle: title });
+      res.send(result);
+  } catch (error) {
+      console.error('Error deleting task:', error);
+      res.status(500).send('Internal Server Error');
+  }
 });
   
 export default router;
